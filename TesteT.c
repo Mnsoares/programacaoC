61833ef48315bc53d26725a9f89aa381ade49825
Mnsoares <mauriciosoares_1987@hotmail.com>
2025-05-09 02:40:16 +0000

    float

--- TesteT.c
+++ TesteT.c
@@ -2,21 +2,25 @@
 
 int main(){
     char nome [20] = "JOGADORES";
-    char Carta [20] = "AA";
+    char Carta [20] = "A1";
     char Estado [20] = "Sergipe";
     char Cidade [20] = "Aracaju";
     float Area = 50.000;
     float Pib = 100.000; 
     int populacao = 100000;
     int Turismo = 8;
+    float Densidade =  5622.24;
+    float PibPerCapita = 44532.91;
 
-    char Cart [20] = "BA";
+    char Cart [20] = "B1";
     char Estad [20] = "Bahia";
     char Cidad [20] = "Salvador";
     float Are = 200.000;
     float Pi = 300.000;
     int populaca = 400000;
     int Turism = 10;
+    float Densidad = 8102.47;
+    float PibPerCapit = 56724.32;
 
 
     printf("%s, mostrem suas cartas!\n", nome);
@@ -28,6 +32,7 @@ int main(){
     printf("PIB: %f\n", Pib);
     printf("População: %d\n", populacao);
     printf("Turismo: %d\n", Turismo);
+
 
     printf("\nCLARA\n");
     printf("\nCarta: %s\n", Cart);
