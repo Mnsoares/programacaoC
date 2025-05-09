31f1170edf8cbb956980f39500e17e55f44ec063
Mnsoares <mauriciosoares_1987@hotmail.com>
2025-05-09 21:35:18 +0000

    Calculos realizados

--- TesteT.c
+++ TesteT.c
@@ -5,22 +5,22 @@ int main(){
     char Carta [20] = "A1";
     char Estado [20] = "Sergipe";
     char Cidade [20] = "Aracaju";
-    float Area = 181.857;
+    float Area = 181857.67;
     float Pib = 16447105.18; 
-    int populacao = 602.757;
+    int populacao = 602757.23;
     int Turismo = 8;
-    float Densidade =  5622.24;  //Densidade real 3 314,5
-    float PibPerCapita = 44532.91; //PIB per Cap real 24 735,91
+    float Densidade = populacao / Area;  //Densidade real 3 314,5
+    float PibPerCapita = Pib / populacao; //PIB per Cap real 24 735,91
 
     char Cart [20] = "B1";
     char Estad [20] = "Bahia";
     char Cidad [20] = "Salvador";
-    float Are = 693.442;
-    float Pi = 62954487.49;
-    int populaca = 2417678;
+    float Are = 693442.89;
+    float pib = 62954487.49;
+    int populaca = 2417678.02;
     int Turism = 10;
     float Densidad = populaca / Are;  //Densidade real 3 486,5
-    float PibPerCapit;  //PIB per Cap real 21 706,06
+    float PibPerCapit = pib/ populaca;  //PIB per Cap real 21 706,06
 
 
     printf("%s, mostrem suas cartas!\n", nome);
@@ -28,21 +28,23 @@ int main(){
     printf("\nCarta: %s\n", Carta);
     printf("Estado: %s\n", Estado);
     printf("Cidade: %s\n", Cidade);
-    printf("Area: %f\n", Area);
-    printf("PIB: %f\n", Pib);
+    printf("Area: %f km²\n", Area);
+    printf("PIB: %f reais\n", Pib);
     printf("População: %d\n", populacao);
     printf("Turismo: %d\n", Turismo);
-
+    printf("Densidade Populacional: %f hab/km²\n", Densidade);
+    printf("PIB Per Capita: %f reais\n", PibPerCapita);
 
     printf("\nCLARA\n");
     printf("\nCarta: %s\n", Cart);
     printf("Estado: %s\n", Estad);
     printf("Cidade: %s\n", Cidad);
-    printf("Area: %f\n", Are);
-    printf("PIB: %f\n", Pi);
+    printf("Area: %f km²\n", Are);
+    printf("PIB: %f reais\n", pib);
     printf("População: %d\n", populaca);
     printf("Turismo: %d\n", Turism);
-    printf("Densidade Populacional: %.2f\n", Densidad);
+    printf("Densidade Populacional: %f hab/km²\n", Densidad);
+    printf("PIB Per Capita: %f reais\n", PibPerCapit);
         
     return 0;
 
