55e21940d9979fde862d97965d2950775479a600
Mnsoares <mauriciosoares_1987@hotmail.com>
2025-05-10 19:14:13 +0000

    operadores logicos

---
+++ Opera_Logico.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main (){
+
+    int numero1, numero2;
+
+    numero1 = 10;
+    numero2 = 5;
+
+    if (numero1 > numero2)
+    {
+        printf("numero1 é maior que numero2!\n");
+    }
+    
+        printf("FORA IF\n");
+
+    int idade = 37;
+    
+    if (idade >= 18)
+    {
+        printf("Você é maior de idade!\n");
+        printf("Fora IF\n");
+    }
+
+    float temperatura = 32.0;
+
+
+    if (temperatura >= 30.0)
+    {
+       printf("Está calor!\n");
+    }
+
+    int nota = 65;
+
+    if (nota >=60)
+    {
+        printf("Parabéns, você passou!  %d\n", nota);
+    }
+    
+    int idade1 = 37;
+    int idade2 = 30;
+
+    if (idade1 > idade2)
+    {
+        printf ("Pessoa1 %d anos, é mais velha que Pessoa2 %d anos! \n", idade1, idade2);
+    }
+    
+    
+    
+
+
+
+   /*  
+   if (condição) {     //abre a chave   
+              //cloco de codigo a ser execultado
+   comando1
+   comando2
+} //fecha a chave
+              */
+
+
+
+
+
+
+
+
+}
+
