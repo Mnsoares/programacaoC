2dca4442ab3c2298304e4ed02d77e9740f409704
Mnsoares <mauriciosoares_1987@hotmail.com>
2025-05-10 20:06:45 +0000

    Temperatura

---
+++ Opera_logico_composto.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main () {
+
+    int numero1;
+    int numero2;
+
+    numero1 = 10;
+    numero2 = 50;
+
+    if(numero1 > numero2) {
+
+    printf("O número1 é maior que o numero2! \n");
+
+    }
+    else {
+
+    printf("O número1 é menor que o número2! \n");
+
+    }
+
+// CODIGO PARA VERIFICAR SE O NÚMERO É PAR OU IMPAR
+
+    int numero = 8;
+
+    if (numero % 2 == 0)
+    {
+        printf("O número é Par! \n");
+    }
+    else 
+    {
+
+    printf("O número é Impar \n");
+    
+    }
+    
+//CODIGO PARA VERIFICAR A TEMPERATURA 
+
+float temperatura = 55.0;
+
+if (temperatura >= 30.0)
+{
+
+    printf("Está Calor! \n");
+
+}
+else 
+{
+
+    printf("Não está Calor! \n");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
