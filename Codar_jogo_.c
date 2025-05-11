5b5af00d54b7dbc0982e56c351155fd77eed3c67
Mnsoares <mauriciosoares_1987@hotmail.com>
2025-05-11 15:14:49 +0000

    jogo funcionando

---
+++ Codar_jogo_.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+    int EscolhaJogador, EscolhaComputador;
+    srand(time(0));
+
+    printf("Jogo De Jokenpô! \n");
+    printf("Escolha Uma Opção! \n");
+    printf("1. Pedra \n");
+    printf("2. Papel \n");
+    printf("3. Tesoura \n");
+    printf("Escolha: \n");   
+    scanf("%d", &EscolhaJogador);
+
+    EscolhaComputador = rand() % 3 + 1;
+
+switch (EscolhaJogador)
+{
+case 1:
+    printf("Jogador: Pedra - ");
+    break;
+case 2:
+    printf("Jogador: Papel - ");
+    break;
+case 3:
+    printf("Jogador: Tesoura - ");
+    break;
+default:
+    printf("Opção Invalida!\n");   
+    break;
+}
+switch (EscolhaComputador)
+{
+case 1:
+    printf("Computador: Pedra \n");
+    break;
+case 2:
+    printf("Computador: Papel \n");
+    break;
+case 3:
+    printf("Computador: Tesoura \n");
+    break;
+default:
+    printf("Opção Invalida!\n");   
+    break;
+}
+
+
+if (EscolhaComputador == EscolhaJogador)
+{
+    printf("***Jogo Empatou!*** \n");
+}else if ((EscolhaJogador == 1) && (EscolhaComputador == 3) || 
+           (EscolhaJogador == 2) && (EscolhaComputador == 1)||
+            (EscolhaJogador == 3) && (EscolhaComputador == 2))
+{
+  printf("***Você Ganhou!*** \n");
+}
+else
+{
+    printf("***Você Perdeu!*** \n ");
+}
+
+
+
+
+
+
+return 0;
+
+
+
+
+}
