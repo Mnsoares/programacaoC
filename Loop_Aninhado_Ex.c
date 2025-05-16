df99c43e80b17e803da4b01d248c5972d7c20adc
Mnsoares <mauriciosoares_1987@hotmail.com>
2025-05-16 02:56:05 +0000

    salvandooo

--- Loop_Aninhado_Ex.c
+++ Loop_Aninhado_Ex.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+// Desenho de um padrão
 int main() {
     int n = 5; // altura do triângulo
  
@@ -9,6 +9,17 @@ int main() {
         }
         printf("\n");
     }
+
+//Tabuada de multiplicação 
+
+     for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
+        printf("\n");
+    }
+   
+
     return 0;
 
 }
